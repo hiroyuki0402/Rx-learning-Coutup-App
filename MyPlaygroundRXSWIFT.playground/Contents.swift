b2dafa0ed72b24e49d18728d6537ac58385a8413
hiroyuki0402 <hiro1990.cloud@gmail.com>
2022-01-19 22:32:12 +0900

    [add] Ex. Rxswift

--- MyPlaygroundRXSWIFT.playground/Contents.swift
+++ MyPlaygroundRXSWIFT.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
+import RxSwift
+import RxCocoa
 
 var greeting = "Hello, playground"
+
+class TestClass {
+
+    var button: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 50, y: 50, width: 200, height: 80)
+        button.addTarget(button, action: #selector(test(sender:)), for: .touchUpInside)
+        return button
+    }()
+
+    var count = 0
+    let deposibag = DisposeBag()
+
+
+
+
+
+    @objc func test(sender: UIButton){
+        count += 1
+        print(count)
+    }
+
+}
+
+let testClass = TestClass()
--- TestRX/ViewController.swift
+++ TestRX/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var label: UILabel!
+    private let reactive = RXSwiftEx()
+
+    var count = 0
+
+    let disposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countUp()
     }
 
+    @IBAction func button(_ sender: Any) {
+        reactive.coutUp()
+    }
+    private func countUp(){
+        ///reactive処理
+        reactive.event
+        /// 通知
+            .subscribe { [unowned self] val in
+                label.text = String(val)
+            }
+            .disposed(by: disposeBag)
+    }
 
+}
+
+class RXSwiftEx {
+    private let subject = PublishSubject<Int>()
+    private var count = 0
+
+    var event: Observable<Int> {
+        return subject
+    }
+
+    func coutUp() {
+        /// countUp
+        count = count + 1
+        /// イベント発行
+        subject.onNext(self.count)
+    }
 }
 
